b7d318aca3a0dd8fcadbfbea5dc1ce6ad036c947
Faisal Bhuiyan <faisalbhuiyan3038@gmail.com>
2023-07-04 17:37:31 -0700

    add exception handling in Login

--- POS/Login.cs
+++ POS/Login.cs
@@ -23,37 +23,47 @@ namespace POS
 
         private void btnLogIn_Click(object sender, EventArgs e)
         {
-            string username = txtUserName.Text;
-            string password = txtPassword.Text;
-            string connectionString = "Data Source=DESKTOP-QFRL6D9\\SQLEXPRESS;Initial Catalog=POSDB;Integrated Security=True";
-
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            try
             {
-                connection.Open();
+                string username = txtUserName.Text;
+                string password = txtPassword.Text;
+                string connectionString = "Data Source=DESKTOP-QFRL6D9\\SQLEXPRESS;Initial Catalog=POSDB;Integrated Security=True";
 
-                string sqlQuery = "SELECT * FROM Admin WHERE UserName = @UserName AND Password = @Password";
-
-                SqlCommand command = new SqlCommand(sqlQuery, connection);
-                command.Parameters.AddWithValue("@UserName", username);
-                command.Parameters.AddWithValue("@Password", password);
-
-                object result = command.ExecuteScalar();
-
-                if (result != null && result != DBNull.Value)
+                using (SqlConnection connection = new SqlConnection(connectionString))
                 {
-                    Tasks tasks = new Tasks();
-                    tasks.Show();
-                    this.Hide();
+                    connection.Open();
+
+                    string sqlQuery = "SELECT * FROM Admin WHERE UserName = @UserName AND Password = @Password";
+
+                    SqlCommand command = new SqlCommand(sqlQuery, connection);
+                    command.Parameters.AddWithValue("@UserName", username);
+                    command.Parameters.AddWithValue("@Password", password);
+
+                    object result = command.ExecuteScalar();
+
+
+                    if (result != null && result != DBNull.Value)
+                    {
+                        Tasks tasks = new Tasks();
+                        tasks.Show();
+                        this.Hide();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Login Failed. Please Try Again.");
+                    }
+
+                     connection.Close();
                 }
-                else
-                {
-                    MessageBox.Show("Login Failed. Please Try Again.");
-                }
-
-                connection.Close();
-
-
             }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An Error occured: " + ex.Message);
+            }
+
+            
+
+            
         }
     }
 }
